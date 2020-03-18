8baf090438057647f70bae4d9198d88f540e808b
gitjiangxb <jiang199424@163.com>
2020-03-19 00:33:31 +0800

    master默认文件

---
+++ src/main/java/test.java
@@ -0,0 +1,9 @@
+/**
+ * @ClassName: test
+ * @Description: TODO
+ * @Author: Jiangxb
+ * @Date: 2020/3/19 0:30
+ * @Version 1.0
+ */
+public class test {
+}
---
+++ src/test/java/testTest.java
@@ -0,0 +1,11 @@
+
+/**
+ * @ClassName: testTest
+ * @Description: TODO
+ * @Author: Jiangxb
+ * @Date: 2020/3/19 0:32
+ * @Version 1.0
+ */
+public class testTest {
+
+}
