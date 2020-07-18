ab36d1271f198f04b9a50dc2f7f10cc238ca7f14
姜小波 <jiang.xiaobo@nxtele.com>
2020-07-18 14:33:39 +0800

    master分支调整为springboot项目

---
+++ src/main/java/com/jxb/monitor_tuning/MasterTestApplication.java
@@ -0,0 +1,13 @@
+package com.jxb.monitor_tuning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasterTestApplication.class, args);
+	}
+
+}
