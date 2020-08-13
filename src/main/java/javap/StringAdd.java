b773bdccf44f4e3631e2bc05021e6b84a5b5bcbd
姜小波 <jiang.xiaobo@nxtele.com>
2020-08-13 20:05:48 +0800

    JVM字节码 分析 字符串+ 操作

---
+++ src/main/java/javap/StringAdd.java
@@ -0,0 +1,33 @@
+package javap;
+
+/**
+ * @ClassName: StringAdd
+ * @Description: TODO 从JVM字节码分析 字符串 + 操作
+ * @Author: Jiangxb
+ * @Date: 2020/08/11 19:52
+ * @Version 1.0
+ **/
+public class StringAdd {
+    public static void main(String[] args) {
+        f1();
+        f2();
+    }
+
+    public static void f1(){
+        String src = "";
+        for (int i = 0 ; i < 10 ; i++ ){
+            // 每一次循环都会new一个StringBuffer
+            src = src + "A";
+        }
+        System.out.println(src);
+    }
+
+    public static void f2(){
+        // 只要一个StringBuffer
+        StringBuffer src = new StringBuffer();
+        for (int i = 0 ; i < 10 ; i++ ){
+            src.append("A");
+        }
+        System.out.println(src);
+    }
+}
